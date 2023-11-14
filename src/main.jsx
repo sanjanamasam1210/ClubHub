982793f291c8755072034b7f3cd23eb1ebbf3873
sanjanamasam1210 <sanjanamasam1210@gmail.com>
2023-11-14 10:51:51 +0530

    some changes

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import App from './App.jsx'
 import './index.css'
 import LoginPage from './pages/LoginPage/LoginPage.jsx'
 import RegisterPage from './pages/RegisterPage/RegisterPage.jsx'
+import HomePage from './pages/HomePage/HomePage.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
-    <LoginPage/>
-    <RegisterPage/>
+    {/* <LoginPage/>
+    <RegisterPage/> */}
     {/* <RegisterPage/> */}
+    <HomePage/>
   </React.StrictMode>,
 )
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './HomePage.scss'
+
+const HomePage = () => {
+  return (
+   <div className='Home'>
+    <div id="header">
+      <div class="head-1"><a href="https://www.google.com/">CLUBHUB</a></div>
+      <div class="head"><a href="https://www.google.com/">HOME</a></div>
+      <div class="head"><a href="https://www.google.com/">CONTACT US</a></div>
+      <div class="head"><a href="https://www.google.com/">DASHBOARD</a></div>
+      <div class="head"><a href="https://www.google.com/">LOGOUT</a></div>
+   </div>
+  </div>
+  )
+}
+
+export default HomePage
+
